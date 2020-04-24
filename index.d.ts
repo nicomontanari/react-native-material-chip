76ca381b9e376e146617c605e1c7f88bbe6dfd64
Nico <nicomontanari.dev@gmail.com>
2020-04-24 19:50:03 +0200

    Added typescript

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,13 @@
-export default interface MaterialChip {
-    text: string
-    checked?: boolean
-    leftIcon?: any
+import { Component } from 'react'
+
+declare module 'react-native-material-chip' {
+    interface MaterialChipProps {
+        text: string
+        checked?: boolean
+        leftIcon?: any
+    }
+
+    export default class MaterialChip extends Component<MaterialChipProps> {
+
+    }
 }
