e1add128f1ed2c85e36f7c436a99a371d7e34861
Nico <nicomontanari.dev@gmail.com>
2020-05-04 11:34:25 +0200

    Fixed margin

--- src/sizes.js
+++ src/sizes.js
@@ -2,7 +2,7 @@ import { PixelRatio } from 'react-native'
 
 export const CHIP_HEIGHT = PixelRatio.roundToNearestPixel(32)
 export const CHIP_RADIUS = PixelRatio.roundToNearestPixel(16)
-export const CHIP_MARGIN = PixelRatio.roundToNearestPixel(14)
+export const CHIP_MARGIN = PixelRatio.roundToNearestPixel(4)
 export const CHIP_TEXT_SIZE = PixelRatio.roundToNearestPixel(14)
 export const CHIP_TEXT_MARGIN = PixelRatio.roundToNearestPixel(16)
 export const CHIP_LEFT_ICON_SIZE = PixelRatio.roundToNearestPixel(24)
